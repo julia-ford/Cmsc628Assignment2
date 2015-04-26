c746c045fca57442b9a3e8ae55884aeec50759bf
Michael Davis <davism4@umbc.edu>
2015-04-25 21:51:24 -0400

    removed mainactivity. app runs and shows map. TODO: get location of device.

--- app/src/main/java/juliaford2015/cmsc628assignment2/MapsActivity.java
+++ app/src/main/java/juliaford2015/cmsc628assignment2/MapsActivity.java
@@ -1,28 +1,116 @@
 package juliaford2015.cmsc628assignment2;
 
+import android.location.Location;
+import android.location.LocationListener;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements LocationListener, OnMapClickListener, OnMapReadyCallback {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private static final String myString = "Me";
+    private static final String destString = "End";
+    private static final String STATE_MY_LOCATION = "MyLocation";
+    private static final String STATE_DESTINATION = "Destination";
+
+    private LatLng myLatLng = null;
+    private LatLng destination = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+        if (savedInstanceState == null) {
+            setMyLatLng();
+        }
+        else {
+            /*
+                it's probably most efficient to try temporarily storing the user's location
+                data. it can be retrieved faster than getting info from the google map.
+             */
+            try {
+                double[] myLoc = savedInstanceState.getDoubleArray(STATE_MY_LOCATION);
+                myLatLng = new LatLng(myLoc[0], myLoc[1]);
+            }
+            catch (Exception e) {
+                setMyLatLng(); // didn't work. try again.
+            }
+            try {
+                double[] destLoc = savedInstanceState.getDoubleArray(STATE_DESTINATION);
+                destination = new LatLng(destLoc[0], destLoc[1]);
+            }
+            catch (Exception e) {
+                // also automatically occurs if user hasn't selected a location yet.
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    /**
+     * uses the google maps API to find the user's location
+     * then those values are read into this app
+     */
+    private void setMyLatLng() {
+        Location myLocation = mMap.getMyLocation();
+        // TODO: can't get location... why?
+        if (myLocation != null) {
+            myLatLng = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(myLatLng).title(myString));
+            Toast.makeText(getApplicationContext(),
+                    "Me: " + myLatLng.latitude + "," + myLatLng.longitude, Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(getApplicationContext(),
+                    "Could not access my location!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onMapClick(LatLng d) {
+        destination = d;
+        mMap.clear();
+        if (myLatLng != null) {
+            mMap.addMarker(new MarkerOptions().position(myLatLng).title(myString));
+        }
+        mMap.addMarker(new MarkerOptions().position(destination).title(destString));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        if (myLatLng != null)
+            savedInstanceState.putDoubleArray(STATE_MY_LOCATION,
+                    new double[]{myLatLng.latitude, myLatLng.longitude});
+        if (destination != null)
+            savedInstanceState.putDoubleArray(STATE_DESTINATION,
+                    new double[]{destination.latitude, destination.longitude});
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //locationManager.removeUpdates(this);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+        //startLocationUpdates();
     }
 
     /**
@@ -51,6 +139,11 @@ public class MapsActivity extends FragmentActivity {
                 setUpMap();
             }
         }
+        if (myLatLng != null) {
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(myLatLng).zoom(20).build();
+            CameraUpdate update = CameraUpdateFactory.newCameraPosition(cameraPosition);
+            mMap.animateCamera(update);
+        }
     }
 
     /**
@@ -60,6 +153,47 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.setOnMapClickListener(this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (myLatLng != null && destination != null) {
+            double latDistance = Math.abs(myLatLng.latitude - destination.latitude);
+            double lonDistance = Math.abs(myLatLng.longitude - destination.longitude);
+            // get squared distance
+            double distance = Math.sqrt(Math.pow(latDistance, 2) + Math.pow(lonDistance, 2));
+            if (distance <= 200) {
+                // ?
+            }
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        setMyLatLng();
+        if (myLatLng != null){
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatLng, 13));
+        }
     }
 }
